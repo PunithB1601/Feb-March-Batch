ad8c3b9ca45f16c8bae463cdf24948606cc0165d
PunithB1601 <punithb072@gmail.com>
2026-08-12 09:10:26 +0530

    validation added to mvc project

--- 12_SpringWebMVCDemo/src/main/java/com/dcl/controller/DemoController.java
+++ 12_SpringWebMVCDemo/src/main/java/com/dcl/controller/DemoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +49,12 @@ public class DemoController {
 	}
 	
 	@PostMapping("/save")
-	public String addUser(@ModelAttribute User user, Model model) {
+	public String addUser(@Validated @ModelAttribute User user, BindingResult result ,Model model) {
+		
+		if(result.hasErrors()) {
+			return "addUser";
+		}
+		
 		User u=uservice.addUser(user);
 		model.addAttribute("success", "Data add Successfully! User ID -"+u.getUserId());
 		return "addUser";
--- 12_SpringWebMVCDemo/src/main/java/com/dcl/entity/User.java
+++ 12_SpringWebMVCDemo/src/main/java/com/dcl/entity/User.java
@@ -1,9 +1,18 @@
 package com.dcl.entity;
 
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +28,16 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer userId;
-	
+	@NotBlank(message = "Username cannot be null!")
 	private String username;
 	
+	
+	@Length(min = 10, max = 10, message = "Phone Number must be of 10 digits!")
+	@NotBlank(message = "Phone number cannot be empty")
 	private String phone;
 	
+	@Email(message = "Please enter the valid email Id")
+	@NotBlank(message = "Email Id cannot be empty!")
+	@Column(unique=true)
 	private String email;
 }
